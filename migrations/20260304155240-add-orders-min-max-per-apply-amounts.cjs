84c8841d3475c20f67573473a66038aae337f90d
Andrew Besedin <andrewprog97@gmail.com>
2026-03-04 19:09:01 +0300

    fix: fix migration column names

--- migrations/20260304155240-add-orders-min-max-per-apply-amounts.cjs
+++ migrations/20260304155240-add-orders-min-max-per-apply-amounts.cjs
@@ -1,15 +1,17 @@
 const ORDERS_TABLE_NAME = 'Orders';
+const MIN_PER_APPLY_AMOUNT_COLUMN_NAME = 'min_per_apply_amount';
+const MAX_PER_APPLY_AMOUNT_COLUMN_NAME = 'max_per_apply_amount';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up (queryInterface, Sequelize) {
-		await queryInterface.addColumn(ORDERS_TABLE_NAME, 'min_apply_amount', {
+		await queryInterface.addColumn(ORDERS_TABLE_NAME, MIN_PER_APPLY_AMOUNT_COLUMN_NAME, {
 			type: Sequelize.DataTypes.STRING,
 			allowNull: true,
 			defaultValue: null,
 		});
 
-		await queryInterface.addColumn(ORDERS_TABLE_NAME, 'max_apply_amount', {
+		await queryInterface.addColumn(ORDERS_TABLE_NAME, MAX_PER_APPLY_AMOUNT_COLUMN_NAME, {
 			type: Sequelize.DataTypes.STRING,
 			allowNull: true,
 			defaultValue: null,
@@ -17,8 +19,8 @@ module.exports = {
 	},
 
 	async down (queryInterface) {
-		await queryInterface.removeColumn(ORDERS_TABLE_NAME, 'min_apply_amount');
+		await queryInterface.removeColumn(ORDERS_TABLE_NAME, MIN_PER_APPLY_AMOUNT_COLUMN_NAME);
 
-		await queryInterface.removeColumn(ORDERS_TABLE_NAME, 'max_apply_amount');
+		await queryInterface.removeColumn(ORDERS_TABLE_NAME, MAX_PER_APPLY_AMOUNT_COLUMN_NAME);
 	}
 };
