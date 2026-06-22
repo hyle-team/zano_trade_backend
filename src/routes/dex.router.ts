736a40b0ff05d5393145ad52fe10b90592ce843a
Andrew Besedin <andrewprog97@gmail.com>
2026-06-22 22:04:26 +0300

    update: comment `/volume-stats` endpoint

--- src/routes/dex.router.ts
+++ src/routes/dex.router.ts
@@ -9,7 +9,7 @@ dexRouter.post('/dex/get-pairs-page', dexController.getPairsPage);
 dexRouter.post('/dex/get-pairs-pages-amount', dexController.getPairsPagesAmount);
 dexRouter.post('/dex/get-pair', dexController.getPair);
 dexRouter.post('/dex/renew-bot', middleware.verifyToken, dexController.registerBot);
-dexRouter.post('/dex/volume-stats', dexController.volumeStats);
+// dexRouter.post('/dex/volume-stats', dexController.volumeStats);
 dexRouter.post(
 	'/dex/get-assets-price-rates',
 	middleware.expressValidator(getAssetsPriceRatesValidator),
